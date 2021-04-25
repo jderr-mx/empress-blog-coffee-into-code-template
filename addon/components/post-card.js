95c0c5677c7cc3d295eecc155d37c29e14e5ea53
John Derr <john.derr@maximos.io>
2021-04-25 11:53:15 -0400

    got markdown parsing working in post summary

--- addon/components/post-card.js
+++ addon/components/post-card.js
@@ -2,8 +2,21 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { htmlSafe } from '@ember/template';
 import layout from '../templates/components/post-card';
+import showdown from 'showdown';
+const summaryLength = 300;
 
-export default Component.extend({
-  layout,
-  tagName: '',
-});
+export default class PostCard extends Component {
+  layout = layout;
+  tagName =  '';
+
+  get contentSummary() {
+  const converter = new showdown.Converter();
+  let content = this.post.content;
+    if(content.length <= summaryLength) {
+      return content
+    } else{
+      let abbrevContent = content.substr(0, content.lastIndexOf(' ', summaryLength))
+      return htmlSafe(converter.makeHtml(abbrevContent) + htmlSafe(' <span class="text-bold text-gray-500">[more]</span>'));
+    }
+  }
+}
